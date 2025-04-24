62b1251f572c15120bdc433b3a371bdba4718028
Vishwas Tarende <vishwastarende@Vishwass-MacBook-Pro.local>
2025-04-25 02:03:29 +0530

    github actions

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath } from "url";
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "dist", // Output directory for production build
+  },
   plugins: [react()],
+  base: "/cuisins/", // Base public path for GitHub Pages deployment
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)), // Proper path alias resolution
     },
   },
 });
